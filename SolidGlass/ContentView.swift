b88f36a08e69b61f4acd058dd84d879712ff5d61
rafaelSwi <rafanswi@protonmail.com>
2025-10-05 16:50:18 -0300

    Initial Commit

---
+++ SolidGlass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolidGlass
+//
+//  Created by Rafael Neuwirth Swierczynski on 05/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolidGlass/SolidGlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidGlassApp.swift
+//  SolidGlass
+//
+//  Created by Rafael Neuwirth Swierczynski on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SolidGlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
